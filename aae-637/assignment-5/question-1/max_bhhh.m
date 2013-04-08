dbb8bf2eebf14e1cfa6e0e28b70094310f363e9b
tdmcarthur <tdm.catcher@gmail.com>
2013-04-08 17:56:18 -0500

    first draft of AAE637 assignment 5

---
+++ aae-637/assignment-5/question-1/max_bhhh.m
@@ -0,0 +1,82 @@
+
+%{
+   ***************************************************************
+   ***************************************************************
+   ***************************************************************
+   ** MAX_BHHH Function -- Maximum likelihood estimation (BHHH) **
+   ***************************************************************
+%}
+function [betas, cov_betas, llf_vec] = max_bhhh(b0, names)
+   global numobs iter_limit critic_limit func_name do_step H ;
+   crit = 1;              % Initialize critical value of % beta change
+   iter = 1;              % Initialize iteration count
+   s = 1;                 % Initialize step length
+   disp('Below is the intermediate iteration information:');     
+   disp('  ');
+   diary on;     % Turn on output to file
+                %******* Begin do loop *********
+   while ((crit > critic_limit) & (iter<= iter_limit));                 
+      z = Grad(b0,func_name,numobs);  % Numerical Gradients          
+      H = z'*z;                       % Compute approx. to Hessian   
+      g = sum(z)';                    % Gradient vector (K x 1)  
+      db = inv(H)*g;                  % Compute full step adjustment 
+      if do_step == 1;                % Use a variable step length 
+       s = 1;                         % Reset base step length 
+       li1 = 0; li2 = 1;              % ** Intialize LLF's under 2 step lengths
+       while (li1 < li2) && (s >=.2); % ** Loop to determine step length
+          li1 = sum(feval(func_name,(b0 + s*db)));  %Eval. LLF at higher SL
+          li2 = sum(feval(func_name,(b0 + s*db/2)));%Eval. LLF at lower SL    
+          s_star=s;
+          s = s/2;          
+       end 
+      else
+          s_star = s;
+      end 
+      betas = b0 + s_star*db;               % Update parameter values            
+       
+      disp('  ');
+      disp('  ');
+      fprintf('i = %3.0f\r', iter);    % Print iteration number
+      fprintf('LLF = %8.4f\r', sum(feval(func_name,betas))); % Print current LLF
+      fprintf('step = %5.3f\r', s_star);    % Print "current" step length
+      fprintf('Parameters \r');        % Print current paramater estimate
+      vv=1;                            % Reset a counter to 1
+    	if length(betas)>7;            % Do we have more than 7 parameters?
+     		numloop=floor(length(betas)./7); % How many group of 7 parameters?
+            resid=length(betas)-numloop*7; % How many parameters
+                                           % after the last group of 7
+     		while vv<=numloop;             % Number of groups of 7
+                fprintf('%8.4f',betas((vv-1)*7+1:vv*7,:)');  % Display 7 parameters 
+    			vv=vv+1;                   % Increment numloop counter
+            end
+            if resid>0;     % After the last group of 7 print out remaining
+                disp('  ');
+                fprintf('%8.4f',betas((vv-1)*7+1:length(b0),:)');
+            end
+        else
+         fprintf('%8.4f',betas');       % Print out all paramatrs if less than 7
+         disp('  ');
+        end 
+        
+      iter = iter + 1;               % Update iteration count
+      crit = abs((betas-b0)./b0)./s_star; % Eval. size adjusted % parm change
+      b0 = betas;                    % Replace old parm vector w/new
+   end
+   llf_vec=feval(func_name,b0); % Vector of Log-Likelihood values
+   cov_betas=inv(H);            % BHHH method for Param. Cov. calculation
+   std = sqrt(diag(cov_betas)); % Compute estimated std. errors
+   zvalue=betas./std;           % t-values 
+   df= numobs-length(b0);              % Degrees of freedom for t-value
+   pvalue=2*(1-normcdf(abs(zvalue)));  % Column vector of param p-values 
+   disp('  ');
+   disp('  ');
+   disp('Final results:');
+   results=horzcat(b0,std,zvalue,pvalue); % Build results matrix
+   table_bwg(names,results,9);
+   disp('  ');
+   
+end
+ 
+    
+    
+
--- econ-710/problem-set-07.m
+++ econ-710/problem-set-07.m
@@ -6,7 +6,7 @@ load 'invest.dat';
 
 %% 8.3.a
 
-invest = horzcat ( invest(:, 1), repmat(1, size(invest, 1), 1), invest(:, 2:end));
+%invest = horzcat( invest(:, 1), repmat(1, size(invest, 1), 1), invest(:, 2:end));
 
 beta = ((invest(:, 2:end)'*invest(:, 2:end))^-1)*invest(:, 2:end)'*invest(:, 1) ;
 
@@ -21,8 +21,8 @@ covb = sighat2*inv(invest(:, 2:end)'*invest(:, 2:end));
 stbls = sqrt(diag(covb));	        
 
 
-paramnames = {'constant', 'tobin_Q','cash_asset','debt_asset'}
-%paramnames = {'tobin_Q','cash_asset','debt_asset'}
+%paramnames = {'constant', 'tobin_Q','cash_asset','debt_asset'}
+paramnames = {'tobin_Q','cash_asset','debt_asset'}
 
 for i=1:length(paramnames)
  fprintf('Param estimate for %s is: %3.4f \n', ...
@@ -46,7 +46,7 @@ end
 wald_stat = beta(2:3)' * inv( [zeros(2,1) eye(2)] * ...
    covb * [zeros(2,1) eye(2)]' ) * beta(2:3);
 
-chi_bwg(0.05,7,fluid_wald_stat);
+chi_bwg(0.05,7,wald_stat);
 
 w_test_p_val = 1 - chi2cdf(wald_stat, 2);
 fprintf('Wald Stat. (H_0: cash_asset = debt_asset = 0): %10.4f \n', wald_stat);
@@ -70,6 +70,522 @@ else
 end 
 
 %% 8.3.d
+
+invest_quad = horzcat( invest(:, 1), ...
+  invest(:, 2).^2, ...
+  invest(:, 3).^2, ...
+  invest(:, 4).^2, ...
+  invest(:, 2) .* invest(:, 3), ...
+  invest(:, 2) .* invest(:, 4), ...
+  invest(:, 3) .* invest(:, 4), ...  
+  );
+
+
+beta = ((invest_quad(:, 2:end)'*invest_quad(:, 2:end))^-1)*invest_quad(:, 2:end)'*invest_quad(:, 1) ;
+
+df = size(invest, 1) - 9;                       
+ehat = invest_quad(:, 1) - invest_quad(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(invest_quad(:, 2:end)'*invest_quad(:, 2:end));          
+
+stbls = sqrt(diag(covb));	
+
+wald_stat = beta(4:end)' * inv( [zeros(6,1) eye(6)] * ...
+   covb * [zeros(6,1) eye(6)]' ) * beta(4:end);
+
+chi_bwg(0.05,7,wald_stat);
+
+w_test_p_val = 1 - chi2cdf(wald_stat, 2);
+fprintf('Wald Stat. (H_0: All interaction and quadratic coef. are zero): %10.4f \n', wald_stat);
+fprintf('Prob Wald Stat. Assum. H_0:            %10.4f \n', w_test_p_val);
+if w_test_p_val < 0.05
+    disp('    There is, therefore, enough evidence to reject H_0');
+else
+    disp('    There is, therefore, not enough evidence to reject H_0');
+end
+
+p. 183
+reg for classes
+
+
+%% 8.4.a
+
+
+urlwrite('http://www.ssc.wisc.edu/~bhansen/710/nerlov.dat','nerlov.dat');
+load 'C:\Users\tdmcarthur\Downloads\nerlov.dat';
+
+nerlov_tranf = horzcat( log(nerlov(:, 1)), ...
+  repmat(1, size(nerlov, 1), 1), ...
+  log(nerlov(:,2)), ...
+  log(nerlov(:,3)), ...
+  log(nerlov(:,4)), ...
+  log(nerlov(:,5)) ...
+  ) ;
+  
+beta = ((nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end))^-1)*nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 1) ;
+
+df = size(nerlov_tranf, 1) - 5;                       
+ehat = nerlov_tranf(:, 1) - nerlov_tranf(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+
+%covb = sighat2*inv(nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end));          
+covb = inv(nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end)) * ...
+  ( nerlov_tranf(:, 2:end)' * diag(ehat.^2)  * nerlov_tranf(:, 2:end) ) * ...
+  inv(nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end)) ... 
+   ;  
+% No DF?
+ 
+
+
+paramnames = {'log_Q','log_PL', 'log_PK', 'log_PF'}
+
+for i=1:length(paramnames)
+ fprintf('Param estimate for %s is: %3.4f \n', ...
+   paramnames{i}, beta(i)   )
+end
+
+for i=1:length(paramnames)
+ fprintf('Standard error for %s is: %3.4f \n', ...
+   paramnames{i}, stbls(i)   )
+end
+
+%% 8.4.c
+
+%b3+b4+b5 = 1
+
+x_mat_temp = nerlov_tranf(:, 2:end);
+r_mat = [zeros(3,2) eye(3)]';
+
+beta_cls = beta - inv(x_mat_temp'*x_mat_temp)*r_mat* ...     
+  inv(r_mat'*inv(x_mat_temp'*x_mat_temp)*r_mat)* ...
+  (r_mat'*beta-1)
+
+
+%% 8.4.d
+
+%b3+b4+b5 = 1
+
+
+beta_emd = beta - covb*r_mat* ...     
+  inv(r_mat'*covb*r_mat)* ...
+  (r_mat'*beta-1)
+
+%% 8.4.e
+
+
+wald_stat = (r_mat'*beta-1)' * inv( r_mat * ...
+   covb * r_mat' ) * (r_mat'*beta-1);
+
+chi_bwg(0.05,7,wald_stat);
+
+w_test_p_val = 1 - chi2cdf(wald_stat, 2);
+fprintf('Wald Stat. (H_0: All interaction and quadratic coef. are zero): %10.4f \n', wald_stat);
+fprintf('Prob Wald Stat. Assum. H_0:            %10.4f \n', w_test_p_val);
+if w_test_p_val < 0.05
+    disp('    There is, therefore, enough evidence to reject H_0');
+else
+    disp('    There is, therefore, not enough evidence to reject H_0');
+end
+	
+
+%% 8.4.f
+
+Test H0 : b3 +b4 +b5 = 1 using a minimum distance statistic
+
+Since the hypothesis is linear, the mimimu distance statistic Jn is the same as the Wald statistic
+
+%% 9.2.a
+
+
+
+
+urlwrite('http://www.ssc.wisc.edu/~bhansen/710/nerlov.dat','nerlov.dat');
+load 'C:\Users\tdmcarthur\Downloads\nerlov.dat';
+
+nerlov_tranf = horzcat( log(nerlov(:, 1)), ...
+  repmat(1, size(nerlov, 1), 1), ...
+  log(nerlov(:,2)), ...
+  log(nerlov(:,3)), ...
+  log(nerlov(:,4)), ...
+  log(nerlov(:,5)), ...
+  log(nerlov(:,2)).^2 ...
+  ) ;
+  
+beta = ((nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end))^-1)*nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 1) ;
+
+df = size(nerlov_tranf, 1) - 6;                       
+ehat = nerlov_tranf(:, 1) - nerlov_tranf(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+
+%covb = sighat2*inv(nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end));          
+covb = inv(nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end)) * ...
+  ( nerlov_tranf(:, 2:end)' * diag(ehat.^2)  * nerlov_tranf(:, 2:end) ) * ...
+  inv(nerlov_tranf(:, 2:end)'*nerlov_tranf(:, 2:end)) ... 
+  ;  
+
+stbls = sqrt(diag(covb));
+tvalue=beta./stbls; 
+pvalue=2*(1-tcdf(abs(tvalue),df)); 
+
+pvalue(6)
+
+%% 9.2.b
+
+%x_mat_temp = nerlov_tranf(:, 2:end);
+%r_mat = [zeros(3,2) eye(3)]';
+
+%beta_cls = beta - inv(x_mat_temp'*x_mat_temp)*r_mat* ...     
+%  inv(r_mat'*inv(x_mat_temp'*x_mat_temp)*r_mat)* ...
+%  (r_mat'*beta-1)
+
+
+nerlov_tranf = horzcat( log(nerlov(:, 1)), ...
+  repmat(1, size(nerlov, 1), 1), ...
+  log(nerlov(:,2)), ...
+  log(nerlov(:,3)), ...
+  log(nerlov(:,4)), ...
+  log(nerlov(:,5)) ...
+  ) ;
+  
+
+
+grid_vals = 2:.1:2.5;
+
+min_b7 = 1000;
+min_b6  = 1000;
+min_sse = 1000;
+min_ehat = 1000;
+
+
+for i=1:length(grid_vals)
+
+  nerlov_nls = horzcat( nerlov_tranf, ...
+    log(nerlov(:,2)) .* (1+exp(-log(nerlov(:,2))-grid_vals(i))).^-1 ...
+    );
+
+
+  beta_nls = ((nerlov_nls(:, 2:end)'*nerlov_nls(:, 2:end))^-1)*nerlov_nls(:, 2:end)'*nerlov_nls(:, 1);
+  
+  x_mat_temp = nerlov_nls(:, 2:end);
+  r_mat = [zeros(3,2) eye(3) zeros(3,1)]';
+
+  beta_cls_nls = beta_nls - inv(x_mat_temp'*x_mat_temp)*r_mat* ...     
+    inv(r_mat'*inv(x_mat_temp'*x_mat_temp)*r_mat)* ...
+    (r_mat'*beta_nls-1);
+  
+  ehat = nerlov_nls(:, 1) - nerlov_nls(:, 2:end) * beta_cls_nls;                          
+  sse = ehat'*ehat;
+  
+  fprintf('%3.1f: %3.5f\n',grid_vals(i), sse)
+  
+  if sse < min_sse
+    min_b7 = grid_vals(i);
+    min_b6 = beta_cls_nls(5);
+    min_sse = sse;
+    min_ehat = ehat;
+  end
+
+end
+
+
+
+%% 9.2.c
+
+% z = Grad(betas,func_name,numobs, dh, x_mat);
+
+m_deriv = horzcat(  ...
+  repmat(1, size(nerlov, 1), 1), ...
+  log(nerlov(:,2)), ...
+  log(nerlov(:,3)), ...
+  log(nerlov(:,4)), ...
+  log(nerlov(:,5)), ...
+  log(nerlov(:,2)) .* (1+exp(-log(nerlov(:,2))-min_b7)).^-1 , ...
+  min_b7 .* log(nerlov(:,2)) .* - ...
+    (nerlov(:,2).*exp(min_b7)./(nerlov(:,2)+exp(min_b7)).^2) ...
+  ) 
+
+
+ 
+covb = inv(m_deriv' * m_deriv) * (m_deriv' * diag(min_ehat.^2) * m_deriv) * inv(m_deriv' * m_deriv);
+
+stbls = sqrt(diag(covb))
+
+
+
+%% 9.3.a.
+
+urlwrite('http://www.ssc.wisc.edu/~bhansen/710/cps78.dat','cps78.dat');
+load 'cps78.dat';
+
+
+cps78_tranf = horzcat(cps78(:,11), cps78(:,[1:10 12:20]) );
+
+
+beta = ((cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end))^-1)*cps78_tranf(:, 2:end)'*cps78_tranf(:, 1) ;
+
+% df = size(invest, 1) - 4; 
+df = size(cps78_tranf, 1) - size(cps78_tranf(:, 2:end), 2);                       
+ehat = cps78_tranf(:, 1) - cps78_tranf(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end));          
+
+stbls = sqrt(diag(covb));	        
+
+
+paramnames = {'ED' 'SOUTH' 'NONWH' 'HISP' 'FE' 'MARR' 'MARRFE' 'EX' 'EXSQ' 'UNION' 'AGE' 'NDEP' 'MANUF' 'CONSTR' 'MANAG' 'SALES' 'CLER' 'SERV' 'PROF'}
+
+for i=1:length(paramnames)
+ fprintf('Param est. and st. error for %6s is: %6.4f  %6.4f \n', ...
+   paramnames{i}, beta(i), stbls(i)   )
+end
+
+
+%% 9.3.b.
+
+
+cps78_tranf = horzcat(cps78_tranf, ...
+  cps78_tranf(:, 2).^2, ...
+  cps78_tranf(:, 2) .* cps78_tranf(:, 6), ...
+  cps78_tranf(:, 2).^2 .* cps78_tranf(:, 6), ...
+  cps78_tranf(:, 6) .* cps78_tranf(:, 13) ...
+);
+
+
+beta = ((cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end))^-1)*cps78_tranf(:, 2:end)'*cps78_tranf(:, 1) ;
+
+% df = size(invest, 1) - 4; 
+df = size(cps78_tranf, 1) - size(cps78_tranf(:, 2:end), 2);                       
+ehat = cps78_tranf(:, 1) - cps78_tranf(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end));          
+
+stbls = sqrt(diag(covb));	
+
+tvalue=beta./stbls; 
+pvalue=2*(1-tcdf(abs(tvalue),df)); 
+
+
+paramnames = {'ED' 'SOUTH' 'NONWH' 'HISP' 'FE' 'MARR' 'MARRFE' 'EX' 'EXSQ' 'UNION' 'AGE' 'NDEP' 'MANUF' 'CONSTR' 'MANAG' 'SALES' 'CLER' 'SERV' 'PROF' 'EDsq' 'EDxFE' 'EDsqxFE' 'NDEPxFE'}
+
+for i=1:length(paramnames)
+ fprintf('Beta, sterr and p-val for %7s is: %6.4f  %6.4f %1.4f \n', ...
+   paramnames{i}, beta(i), stbls(i), pvalue(i)   )
+end
+
+
+%% 9.3.c.
+
+
+cps78_tranf = cps78_tranf(:, [1:2 4 6:17 19:24]);
+
+
+beta = ((cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end))^-1)*cps78_tranf(:, 2:end)'*cps78_tranf(:, 1) ;
+
+% df = size(invest, 1) - 4; 
+df = size(cps78_tranf, 1) - size(cps78_tranf(:, 2:end), 2);                       
+ehat = cps78_tranf(:, 1) - cps78_tranf(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end));          
+
+stbls = sqrt(diag(covb));	
+
+tvalue=beta./stbls; 
+pvalue=2*(1-tcdf(abs(tvalue),df)); 
+
+
+paramnames = {'ED' 'NONWH' 'FE' 'MARR' 'MARRFE' 'EX' 'EXSQ' 'UNION' 'AGE' 'NDEP' 'MANUF' 'CONSTR' 'MANAG' 'SALES'  'SERV' 'PROF' 'EDsq' 'EDxFE' 'EDsqxFE' 'NDEPxFE'};
+
+for i=1:length(paramnames)
+ fprintf('Beta, sterr and p-val for %7s is: %6.4f  %6.4f %1.4f \n', ...
+   paramnames{i}, beta(i), stbls(i), pvalue(i)   )
+end
+
+
+%% 9.3.d.
+
+
+z_mat = horzcat( ...
+  repmat(1, size(cps78_tranf, 1), 1), ...
+  cps78_tranf(:, 4) ...
+  );
+
+alpha_tilde = inv(z_mat'*z_mat) * z_mat' * ehat.^2;
+% Skedastic regression
+
+df = size(z_mat, 1) - size(z_mat, 2);                       
+ehat_sked = cps78_tranf(:, 1) - z_mat * alpha_tilde;                          
+sse = ehat_sked'*ehat_sked;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(z_mat'*z_mat);          
+
+stbls = sqrt(diag(covb));	
+tvalue=alpha_tilde(2)./stbls(2);                      
+pvalue=2*(1-tcdf(abs(tvalue),df)) 
+
+
+%% 9.3.e.
+
+z_mat = horzcat( ...
+  repmat(1, size(cps78_tranf, 1), 1), ...
+  cps78_tranf(:, 3) ...
+  );
+
+alpha_tilde = inv(z_mat'*z_mat) * z_mat' * ehat.^2;
+% Skedastic regression
+
+df = size(z_mat, 1) - size(z_mat, 2);                       
+ehat_sked = cps78_tranf(:, 1) - z_mat * alpha_tilde;                          
+sse = ehat_sked'*ehat_sked;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(z_mat'*z_mat);          
+
+stbls = sqrt(diag(covb));	
+tvalue=alpha_tilde(2)./stbls(2);                      
+pvalue=2*(1-tcdf(abs(tvalue),df)) 
+
+%% 9.3.f.
+
+z_mat = horzcat( ...
+  repmat(1, size(cps78_tranf, 1), 1), ...
+  cps78_tranf(:, 7).^2, ...
+  cps78_tranf(:, 10).^2, ...
+  cps78_tranf(:, 11).^2 ...
+  );
+
+alpha_tilde = inv(z_mat'*z_mat) * z_mat' * ehat.^2;
+% Skedastic regression
+
+sigma_tilde = z_mat * alpha_tilde;
+
+sigma_tilde( sigma_tilde < sighat2 * .1) = sighat2 * .1;
+
+
+df = size(z_mat, 1) - size(z_mat, 2);                       
+ehat_sked = cps78_tranf(:, 1) - z_mat * alpha_tilde;                          
+sse = ehat_sked'*ehat_sked;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(z_mat'*z_mat);          
+
+stbls = sqrt(diag(covb));	
+
+wald_stat = alpha_tilde(2:4)' * inv( [zeros(3,1) eye(3)] * ...
+   covb * [zeros(3,1) eye(3)]' ) * alpha_tilde(2:4);
+
+chi_bwg(0.05,7,wald_stat);
+
+w_test_p_val = 1 - chi2cdf(wald_stat, 2);
+fprintf('Wald Stat. (H_0: cash_asset = debt_asset = 0): %10.4f \n', wald_stat);
+fprintf('Prob Wald Stat. Assum. H_0:            %10.4f \n', w_test_p_val);
+if w_test_p_val < 0.05
+    disp('    There is, therefore, enough evidence to reject H_0');
+else
+    disp('    There is, therefore, not enough evidence to reject H_0');
+end
+
+
+%% 9.3.g.
+
+
+
+beta = ((cps78_tranf(:, 2:end)'*diag(sigma_tilde)*cps78_tranf(:, 2:end))^-1)*cps78_tranf(:, 2:end)'*diag(sigma_tilde)*cps78_tranf(:, 1);
+
+
+% df = size(invest, 1) - 4; 
+df = size(cps78_tranf, 1) - size(cps78_tranf(:, 2:end), 2);                       
+ehat = cps78_tranf(:, 1) - cps78_tranf(:, 2:end) * beta;                          
+sse = ehat'*ehat;                         
+sighat2 = sse/df;                         
+  
+covb = sighat2*inv(cps78_tranf(:, 2:end)'*cps78_tranf(:, 2:end));          
+
+stbls = sqrt(diag(covb));	
+
+tvalue=beta./stbls; 
+pvalue=2*(1-tcdf(abs(tvalue),df)); 
+
+
+paramnames = {'ED' 'NONWH' 'FE' 'MARR' 'MARRFE' 'EX' 'EXSQ' 'UNION' 'AGE' 'NDEP' 'MANUF' 'CONSTR' 'MANAG' 'SALES'  'SERV' 'PROF' 'EDsq' 'EDxFE' 'EDsqxFE' 'NDEPxFE'};
+
+for i=1:length(paramnames)
+ fprintf('Beta, sterr and p-val for %7s is: %6.4f  %6.4f %1.4f \n', ...
+   paramnames{i}, beta(i), stbls(i), pvalue(i)   )
+end
+
+
+%% 9.3.h.
+
+
+
+
+
+
+
+EX age ndep
+
+
+
+
+SOUTH
+HISP
+CLER
+
+
+
+
+
+ED sq
+ED and FE interact
+ED sq and FE interact
+
+NDEP  and FE interact
+
+
+
+
+
+
+
+http://www.ssc.wisc.edu/~bhansen/710/cps78.pdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+paramnames = {'log_Q','log_PL', 'log_PK', 'log_PF'}
+
+for i=1:length(paramnames)
+ fprintf('Param estimate for %s is: %3.4f \n', ...
+   paramnames{i}, beta(i)   )
+end
+
+for i=1:length(paramnames)
+ fprintf('Standard error for %s is: %3.4f \n', ...
+   paramnames{i}, stbls(i)   )
+end
+
+
+
 
 
 
